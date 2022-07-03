7b1fe4087cd4d224c35b03baa828d98b30440a39
flixcrls <carlos.felix200084@gmail.com>
2022-07-03 22:39:33 +0700

    final commit

---
+++ app/src/main/java/com/felix/newsapp/repository/NewsRepository.kt
@@ -0,0 +1,21 @@
+package com.felix.newsapp.repository
+
+import com.felix.newsapp.api.RetrofitInstance
+import com.felix.newsapp.db.ArticleDatabase
+import com.felix.newsapp.models.Article
+
+class NewsRepository(
+    val db: ArticleDatabase
+) {
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
+
+    suspend fun searchNews(searchQuery: String, pageNumber: Int) =
+        RetrofitInstance.api.searchForNews(searchQuery, pageNumber)
+
+    suspend fun upsert(article: Article) = db.getArticleDao().upsert(article)
+
+    fun getSavedNews() = db.getArticleDao().getAllArticles()
+
+    suspend fun deleteArticle(article: Article) = db.getArticleDao().deleteArticle(article)
+}
--- app/src/main/java/com/felix/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/felix/newsapp/ui/NewsActivity.kt
@@ -2,15 +2,25 @@ package com.felix.newsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.felix.newsapp.R
+import com.felix.newsapp.db.ArticleDatabase
+import com.felix.newsapp.repository.NewsRepository
 import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
+
+    lateinit var viewModel: NewsViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
+        viewModel = ViewModelProvider(this, viewModelProviderFactory)[NewsViewModel::class.java]
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
